59c65608c238da0345f4015c71de25286b907cd7
Shubham <shubh.manu44@gmail.com>
2022-07-28 15:57:19 +0530

    First

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First install the dependencies:
+
+```
+npm i
+```
+
+Then, run the development server:
+```
+npm run dev
+```
+
+
---
+++ truffle-config.js
@@ -0,0 +1,18 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0"
+    }
+  }
+};
