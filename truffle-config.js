f3e34353bacbdf18731ce00016b63780956e8375
Shubham <shubh.manu44@gmail.com>
2022-07-28 16:42:52 +0530

    updated mnemonic

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = "5f539687e398384288a8d182c48ce9336a6284668f33759c1068b071ce743d73";
 
 module.exports = {
   networks: {
